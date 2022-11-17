d2d53a76899badedd91af56fe2cba3a1acf367ad
inderharrysingh <inderjotsingh141@gmail.com>
2022-11-17 17:07:47 +0530

    adding postgres database instead of sqlite

--- main.py
+++ main.py
@@ -8,9 +8,12 @@ from sqlalchemy.orm import relationship
 from flask_login import UserMixin, login_user, LoginManager, login_required, current_user, logout_user
 from forms import CreatePostForm, Register, Login_form, Comment
 from flask_gravatar import Gravatar
-
+import os
 app = Flask(__name__)
-app.config['SECRET_KEY'] = '8BYkEfBA6O6donzWlSihBXox7C0sKR6b'
+if os.environ.get('SECRET_KEY') == None:
+    app.config['SECRET_KEY'] = "8BYkEfBA6O6donzWlSihBXox7C0sKR6b"
+else:
+    app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY')
 ckeditor = CKEditor(app)
 Bootstrap(app)
 gravatar = Gravatar(app,
@@ -23,7 +26,12 @@ gravatar = Gravatar(app,
                     base_url=None)
 
 ##CONNECT TO DB
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///blog.db'
+##if sqlite:///blog.db\8BYkEfBA6O6donzWlSihBXox7C0sKR6b
+if os.environ.get('DATABASE_URL') == None:
+    app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///blog.db"
+
+else:
+    app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
